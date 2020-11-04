a9f53a732fee47a08ebdd6ff25a91633025915ca
Arsen Krochak <60494717+d3b0nair@users.noreply.github.com>
2020-11-03 18:49:00 -0600

    Implementing API requests

--- components/App.js
+++ components/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import unsplashAPI from "../API/unsplashAPI";
 import SearchBar from "./SearchBar";
+
 class App extends React.Component {
-  onSearchSubmit(input){
-    console.log(input);
+  state = { images: [] };
+  onSearchSubmit = async (input) => {
+    const res = await unsplashAPI.get("/search/photos", {
+      params: {
+        query: input,
+      },
+    });
+    this.setState({ images: res.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
       </div>
     );
   }
